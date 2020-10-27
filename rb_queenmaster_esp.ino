b2b94693f2792492a157f4f904559e238b855a1a
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-10-27 21:15:58 +0700

    setting wifi access point

--- rb_queenmaster_esp.ino
+++ rb_queenmaster_esp.ino
@@ -1,10 +1,13 @@
 #include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
 
 const String PROJECT_VERSION = "0.0.0";
 
 void setup() {
   commonSetting();
   loadVersion();
+  wifiSetting();
 }
 
 void loop() {
@@ -15,7 +18,18 @@ void commonSetting() {
   Serial.begin(115200);
 }
 
+void wifiSetting() {
+  // Access Point Setup
+  Serial.println();
+  Serial.println("Configuring Access Point...");
+  WiFi.softAP("SCANDO_hardware", "scandohardware");
+  IPAddress hardwareIPAddress = WiFi.softAPIP();
+  Serial.print("Hardware IP Address: ");
+  Serial.println(hardwareIPAddress);
+}
+
 void loadVersion() {
+  // Printing header and version
   Serial.println();
   Serial.println("============================");
   Serial.println("     RB_QueenMaster_Esp");
