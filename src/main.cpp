0156e95b16b4b640d5ec88015282223547bf9484
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-10-28 12:37:30 +0700

    fix indentation on main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,45 +5,50 @@
 
 #define PROJECT_VERSION "0.0.1"
 
-void commonSetting() {
-  Serial.begin(115200);
-  ProjectHeader::loadHeader(PROJECT_VERSION);
+void commonSetting()
+{
+    Serial.begin(115200);
+    ProjectHeader::loadHeader(PROJECT_VERSION);
 }
 
-void wifiSetting() {
-  // Set WiFi to both mode
-  Serial.println("Configuring WiFi Mode...");
-  WiFi.mode(WIFI_AP_STA);
-  delay(100);
-  
-  // Access Point Setup
-  Serial.println("Configuring Access Point...");
-  WiFi.softAP("SCANDO_hardware", "scandohardware");
-  IPAddress hardwareIPAddress = WiFi.softAPIP();
-  Serial.println();
-  Serial.print("Hardware IP Address: ");
-  Serial.println(hardwareIPAddress);
+void wifiSetting()
+{
+    // Set WiFi to both mode
+    Serial.println("Configuring WiFi Mode...");
+    WiFi.mode(WIFI_AP_STA);
+    delay(100);
 
-  // WiFi Client Setup
-  WiFi.begin("Ricki", "rickibinyamin");
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println("Ricki");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+    // Access Point Setup
+    Serial.println("Configuring Access Point...");
+    WiFi.softAP("SCANDO_hardware", "scandohardware");
+    IPAddress hardwareIPAddress = WiFi.softAPIP();
+    Serial.println();
+    Serial.print("Hardware IP Address: ");
+    Serial.println(hardwareIPAddress);
+
+    // WiFi Client Setup
+    WiFi.begin("Ricki", "rickibinyamin");
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println("Ricki");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
-void setup() {
-  commonSetting();
-  wifiSetting();
+void setup()
+{
+    commonSetting();
+    wifiSetting();
 }
 
-void loop() {
-  Serial.println("HALO");
-  delay(1000);
+void loop()
+{
+    Serial.println("HALO");
+    delay(1000);
 }
