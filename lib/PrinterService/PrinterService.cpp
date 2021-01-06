a9bf180e302b24f07e0f093a3876da1104358cc6
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2021-01-06 14:12:00 +0700

    add additional setting on init

--- lib/PrinterService/PrinterService.cpp
+++ lib/PrinterService/PrinterService.cpp
@@ -11,6 +11,18 @@
 // MARK: - Life Cycles
 
 PrinterService::PrinterService(int busy, int strobe, int D7, int D6, int D5, int D4, int D3, int D2, int D1, int D0) {
+    
+    _busy = busy;
+    _strobe = strobe;
+    _D7 = D7;
+    _D6 = D6;
+    _D5 = D5;
+    _D4 = D4;
+    _D3 = D3;
+    _D2 = D2;
+    _D1 = D1;
+    _D0 = D0;
+
     pinMode(busy, INPUT_PULLUP);
     pinMode(strobe, OUTPUT);
     pinMode(D7, OUTPUT);
@@ -21,7 +33,19 @@ PrinterService::PrinterService(int busy, int strobe, int D7, int D6, int D5, int
     pinMode(D2, OUTPUT);
     pinMode(D1, OUTPUT);
     pinMode(D0, OUTPUT);
+
+    digitalWrite(strobe, HIGH);
 }
 
 PrinterService::~PrinterService() {
+}
+
+// MARK: - Private Method
+
+void PrinterService::_parallelizeData(unsigned char data) {
+
+}
+
+void PrinterService::_tickStrobe() {
+
 }
