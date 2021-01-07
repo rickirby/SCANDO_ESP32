23b0d5527dbd0eefc75a293ac89a0873c99939a5
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2021-01-07 17:12:50 +0700

    adding printerservice instance on main

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <PROJECT_CONSTANT.h>
 #include <PrinterService.hpp>
 
+PrinterService* printerService;
 WifiManager* wifiManager;
 
 void commonSetting() {
@@ -16,15 +17,20 @@ void commonSetting() {
     ProjectSetting::checkIfFactoryResetTriggered();
 }
 
+void configurePrinterService() {
+    printerService = new PrinterService(35, 34, 13, 12, 14, 27, 26, 25, 33, 32);
+}
+
 void wifiSetting() {
     String ssid = (String)"BP" + (String)HARDWARE_PRODUCT_ID;
     String pass = (String)"pass" + (String)HARDWARE_PRODUCT_ID;
-    wifiManager = new WifiManager((char*)ssid.c_str(), (char*)pass.c_str());
+    wifiManager = new WifiManager((char*)ssid.c_str(), (char*)pass.c_str(), printerService);
     wifiManager->begin();
 }
 
 void setup() {
     commonSetting();
+    configurePrinterService();
     wifiSetting();
 }
 
