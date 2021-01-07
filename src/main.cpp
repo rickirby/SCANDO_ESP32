d25c5436a4ae5d545206c5029fe5a599b65f5b0c
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2021-01-07 23:17:40 +0700

    implement multithread

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,9 @@
 PrinterService* printerService;
 WifiManager* wifiManager;
 
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
 void commonSetting() {
     Serial.begin(115200);
     delay(1000);
@@ -28,12 +31,49 @@ void wifiSetting() {
     wifiManager->begin();
 }
 
+void Task1code(void* pvParameters ){
+    while(true) {
+        wifiManager->checkWifiStatus();
+    }
+}
+
+void Task2code(void* pvParameters ){
+    while(true) {
+        Serial.println("Task2");
+        delay(300);
+    }
+}
+
+void configureTask() {
+    //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
+    xTaskCreatePinnedToCore(
+        Task1code,   /* Task function. */
+        "Task1",     /* name of task. */
+        10000,       /* Stack size of task */
+        NULL,        /* parameter of the task */
+        1,           /* priority of the task */
+        &Task1,      /* Task handle to keep track of created task */
+        0);          /* pin task to core 0 */
+        delay(500); 
+        
+    //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
+    xTaskCreatePinnedToCore(
+        Task2code,   /* Task function. */
+        "Task2",     /* name of task. */
+        10000,       /* Stack size of task */
+        NULL,        /* parameter of the task */
+        1,           /* priority of the task */
+        &Task2,      /* Task handle to keep track of created task */
+        1);          /* pin task to core 1 */
+        delay(500); 
+}
+
 void setup() {
     commonSetting();
     configurePrinterService();
     wifiSetting();
+    configureTask();
 }
 
 void loop() {
-    wifiManager->checkWifiStatus();
 }
