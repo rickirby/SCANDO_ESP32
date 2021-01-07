b17c9ec2a710c98d30713545e49398a89ac1c524
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2021-01-07 23:21:44 +0700

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -45,27 +45,11 @@ void Task2code(void* pvParameters ){
 }
 
 void configureTask() {
-    //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
-    xTaskCreatePinnedToCore(
-        Task1code,   /* Task function. */
-        "Task1",     /* name of task. */
-        10000,       /* Stack size of task */
-        NULL,        /* parameter of the task */
-        1,           /* priority of the task */
-        &Task1,      /* Task handle to keep track of created task */
-        0);          /* pin task to core 0 */
-        delay(500); 
-        
-    //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
-    xTaskCreatePinnedToCore(
-        Task2code,   /* Task function. */
-        "Task2",     /* name of task. */
-        10000,       /* Stack size of task */
-        NULL,        /* parameter of the task */
-        1,           /* priority of the task */
-        &Task2,      /* Task handle to keep track of created task */
-        1);          /* pin task to core 1 */
-        delay(500); 
+    xTaskCreatePinnedToCore(Task1code, "Task1", 10000, NULL, 1, &Task1, 0);
+    delay(500); 
+    
+    xTaskCreatePinnedToCore(Task2code, "Task2", 10000, NULL, 1, &Task2, 1);          /* pin task to core 1 */
+    delay(500); 
 }
 
 void setup() {
